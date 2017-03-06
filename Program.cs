1cb4a125c8bca3e55d55516bf3c049ccf25dc126
Alexander Voronov <voronov@yandex-team.ru>
2017-03-06 18:46:15 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Palindrome
+{
+    internal class Program
+    {
+        private static bool debug = true;
+
+        public static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Enter line, please (or 'quit' for exit)");
+                var raw_str = Console.ReadLine();
+
+                _log("Raw string is '" + raw_str + "'");
+
+                if (raw_str.Equals("quit")) break;
+
+                var str = Regex.Replace(raw_str.ToLower(), "[^a-za-я]", String.Empty);
+
+                _log("Sanitized string is '" + str + "'");
+
+                if (isPalindrome(str))
+                {
+                    Console.WriteLine(String.Format("String '{0}' is a palindrome", str));
+                }
+                else
+                {
+                    Console.WriteLine(String.Format("String '{0}' is not a palindrome", str));
+                }
+            }
+        }
+
+        private static bool isPalindrome(string str)
+        {
+            if (str.Length < 1) return false;
+
+            var i = 0;
+            var j = str.Length - 1;
+            while (i <= j)
+            {
+                _log("str[" + j + "] = " + str[j]);
+                _log("str[" + i + "] = " + str[i]);
+
+                if (str[i] != str[j]) return false;
+
+                i++;
+                j--;
+            }
+            return true;
+        }
+
+        private static void _log(string str)
+        {
+            if (debug) Console.WriteLine("DEBUG: " + str);
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,26 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+
+[assembly: AssemblyTitle("Palindrome")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Palindrome")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+
+[assembly: Guid("396A4AB7-BE3D-452F-A103-C7CD27C067EE")]
